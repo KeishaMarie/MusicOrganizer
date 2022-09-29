2c916387ba8e9049f666f49f506085c236b2b3bd
Keisha Hepner <kmhepner90@gmail.com>
2022-09-29 14:01:32 -0700

    Pass tests for Get Id and Clear All
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- MusicOrganizer.Tests/ModelTests/ArtistTests.cs
+++ MusicOrganizer.Tests/ModelTests/ArtistTests.cs
@@ -6,13 +6,47 @@ using System;
 namespace MusicOrganizer.Tests
 {
   [TestClass]
-  public class ArtistTests
+  public class ArtistTests : IDisposable
   {
+    
+    public void Dispose()
+    {
+      Artist.ClearAll();
+    }
+
     [TestMethod]
     public void ArtistConstructor_CreatesInstanceOfArtist_Artist()
     {
       Artist newArtist = new Artist("test artist");
       Assert.AreEqual(typeof(Artist), newArtist.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Category";
+      Artist newArtist = new Artist(name);
+
+      //Act
+      string result = newArtist.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsArtistId_Int()
+    {
+      //Arrange
+      string name = "Test Artist";
+      Artist newArtist = new Artist(name);
+
+      //Act
+      int result = newArtist.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
     }
   }
 }
